2605a501c987baabcd3158048ef21bcd69003840
nuricz <nuria.czajko@gmail.com>
2020-11-12 16:42:02 -0300

    Delete date column from todos

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_141043) do
+ActiveRecord::Schema.define(version: 2020_11_12_193028) do
 
   create_table "todos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
     t.boolean "completed", default: false, null: false
-    t.date "date", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
