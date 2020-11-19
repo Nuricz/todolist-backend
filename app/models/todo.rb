63dc8ab376ff383216e677a03c38cc3036acb649
nuricz <nuria.czajko@gmail.com>
2020-11-19 08:37:10 -0300

    Created method to display creation time the  todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,21 @@
 class Todo < ApplicationRecord
+
+    def created_ago()
+        seconds = Time.now - self.created_at
+        minutes = (seconds/60).to_int
+        hours = minutes/60
+        minutes = minutes%60
+        formato = "" << "Creado hace "+hours.to_s
+        if(hours == 1)
+            formato << " hora y "+minutes.to_s
+        else
+            formato << " horas y "+minutes.to_s
+        end
+        if(minutes == 1)
+            formato << " minuto"
+        else
+            formato << " minutos"
+        end 
+        formato
+    end
 end
